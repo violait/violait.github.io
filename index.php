d5e56c895ecdfdaf96fbb78ead21b1c36f6a7582
violait <31415586+violait@users.noreply.github.com>
2017-08-28 20:19:16 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <title>Виолайт</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Фабрика Виолайт">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    Матрасы и наматрасники Виолайт!
+  </body> 
+</html>
